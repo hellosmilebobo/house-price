7c34f39b95bdda667335c716d0953ae7ff785855
hellosmilebobo <89713983+hellosmilebobo@users.noreply.github.com>
2025-03-13 23:59:16 +0000

    script.js

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ function calculateROI() {
       let totalExpense = yearlyLoanPayment + totalServiceCharge;
       let buyFlatSaveMoney = yearlyRent - totalExpense;
       let savingInterest = buyFlatSaveMoney * depositRate;
-      let depositLostInterest = deposit * depositRate;
-      let flatPriceRaiseProfit = flatPrice * inflationRate;
+      let depositLostInterest = deposit * (1+depositRate)^(year-1)*depositRate;
+      let flatPriceRaiseProfit = flatPrice * (1+inflationRate)*(year-1)*inflationRate;
       let principalPaid = yearlyLoanPayment - (loanAmount * loanRate);
       let balance = buyFlatSaveMoney + savingInterest - depositLostInterest + flatPriceRaiseProfit + principalPaid;
       
